8238c6c0ae818df6a34df91d57f9ac0ecac44a6c
Kenneth Kutschera <kennethchoi19@gmail.com>
2019-06-03 04:51:08 +0000

    Automatically backed up by Learn

--- app/models/concerns/post_validator.rb
+++ app/models/concerns/post_validator.rb
@@ -1,3 +1,18 @@
 class PostValidator < ActiveModel::Validator
+<<<<<<< HEAD
 
+=======
+    def is_clickbait?
+        click_bait = [
+          /Won't Believe/i,
+          /Secret/i,
+          /Top [0-9]*/i,
+          /Guess/i
+        ]
+
+        if click_bait.none? { |phrase| phrase.match title }
+            errors.add(:title, "must be clickbait!")
+        end
+    end
+>>>>>>> 7099f5e16b1ade6971d1e8fc958a302a24fe49e5
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,14 @@
 class Post < ActiveRecord::Base
+<<<<<<< HEAD
     
+=======
+    validate :is_clickbait?
+>>>>>>> 7099f5e16b1ade6971d1e8fc958a302a24fe49e5
     validates :title, presence: true
     validates :content, length: { in: 250..500}
     validates :summary, length: { maximum: 250 }
     validates :category, inclusion: { in: %w( Fiction Non-Fiction )}
+<<<<<<< HEAD
     validate :is_clickbait?
 
     private
@@ -20,4 +25,6 @@ class Post < ActiveRecord::Base
             errors.add(:title, "must be clickbait!")
         end
     end
+=======
+>>>>>>> 7099f5e16b1ade6971d1e8fc958a302a24fe49e5
 end
